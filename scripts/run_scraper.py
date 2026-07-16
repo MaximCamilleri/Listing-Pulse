1a592f7eb3ad6dc7890b2bcf3aff49c6d14a78e4
Max Camilleri <max.camilleri.20@um.edu.mt>
2026-07-16 21:13:56 +0200

    import bug fix

--- scripts/run_scraper.py
+++ scripts/run_scraper.py
@@ -8,7 +8,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
-from src.service.scraper_service import ScraperService
+from src.control.scraper_service import ScraperService
 from src.support.logger import configure_logging, get_logger
 
 
